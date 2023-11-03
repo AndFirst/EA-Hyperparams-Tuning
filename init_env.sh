a83840d9273450bca2278b495c31a8a553260342
iokninsk <01158211@pw.edu.pl>
2023-11-03 21:01:18 +0100

    Add script which init environment and install CEC2017

---
+++ init_env.sh
@@ -0,0 +1,6 @@
+python3 -m venv venv
+source ./venv/bin/activate
+git clone https://github.com/tilleyd/cec2017-py
+cd cec2017-py
+pip install setuptools
+python3 setup.py install
---
+++ main.py
@@ -0,0 +1,4 @@
+from cec2017.functions import f1
+
+if __name__ == "__main__":
+    pass
