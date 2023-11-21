5a0f3a1eab4ed1ccc1c94f298701c26e80b8d184
Gephyte <idab@vp.pl>
2023-11-21 16:11:04 +0100

    Update environment.py

--- environment.py
+++ environment.py
@@ -22,65 +22,65 @@ def _to2DSpace(space: tuple[int, int], coord: int) -> tuple[int, int]:
 
 class EvolutionaryEnv(gym.Env):
     def __init__(self, max_steps: int) -> None:
-        self.action_dim = (len(CrossingType), 5)
-        self.observation_dim = (10, 100)
+        self._action_dim = (len(CrossingType), 5)
+        self._observation_dim = (10, 100)
         
-        self.success_bins = np.linspace(0, 1, num=self.observation_dim[0] - 1)
-        self.distance_bins = np.linspace(0, 10e10, num=self.observation_dim[1] - 1)
-        self.mutation_probabilities = np.linspace(0, 1, num=self.action_dim[1])
+        self._success_bins = np.linspace(0, 1, num=self._observation_dim[0] - 1)
+        self._distance_bins = np.linspace(0, 10e10, num=self._observation_dim[1] - 1)
+        self._mutation_probabilities = np.linspace(0, 1, num=self._action_dim[1])
 
-        self.action_space = gym.spaces.Discrete(self.action_dim[0] * self.action_dim[1])
-        self.observation_space = gym.spaces.Discrete(self.observation_dim[0] * self.observation_dim[1])
+        self.action_space = gym.spaces.Discrete(self._action_dim[0] * self._action_dim[1])
+        self.observation_space = gym.spaces.Discrete(self._observation_dim[0] * self._observation_dim[1])
 
-        self.max_steps = max_steps
-        self.current_step = 0
-        self.population = generate_start_population(200, 10, -50, 50) # aktualny stan algorytmu ewolucyjnego
+        self._max_steps = max_steps
+        self._current_step = 0
+        self._population = generate_start_population(200, 10, -50, 50) # aktualny stan algorytmu ewolucyjnego
 
         self.best_quality = 0
-        self.current_success = 0
-        self.current_distance = np.digitize(calculate_avg_distance(self.population), self.distance_bins)
+        self._current_success = 0
+        self._current_distance = np.digitize(calculate_avg_distance(self._population), self._distance_bins)
 
     def step(self, action: Any) -> tuple[Any, SupportsFloat, bool, bool, dict[str, Any]]:
         
         
-        selected_crossover, selected_mutation = _to2DSpace(self.action_dim, action)
+        selected_crossover, selected_mutation = _to2DSpace(self._action_dim, action)
 
         _, self.best_quality, success_percent, avg_distance = evolutionary_algorithm(f=f7,
-                                    population=self.population,
+                                    population=self._population,
                                     mutation_value=50.,
-                                    mutation_probability=self.mutation_probabilities[selected_mutation],
+                                    mutation_probability=self._mutation_probabilities[selected_mutation],
                                     elite_size=20,
-                                    t_max=20,
+                                    t_max=10,
                                     crossing_type=CrossingType(selected_crossover),
                                     crossing_probability=0
                                     )
         
-        success = np.digitize(success_percent, self.success_bins)
-        distance = np.digitize(avg_distance, self.distance_bins)
+        success = np.digitize(success_percent, self._success_bins)
+        distance = np.digitize(avg_distance, self._distance_bins)
         
         reward = self._calculate_reward(success, distance)
-        terminated = self.current_step >= self.max_steps
+        terminated = self._current_step >= self._max_steps
 
-        self.current_success = success
-        self.current_distance = distance
-        self.current_step += 1
+        self._current_success = success
+        self._current_distance = distance
+        self._current_step += 1
 
-        return _to1DSpace(self.observation_dim, (self.current_success, self.current_distance)), reward, terminated, False, {}
+        return _to1DSpace(self._observation_dim, (self._current_success, self._current_distance)), reward, terminated, False, {}
     
     def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[Any, dict[str, Any]]:
-        self.current_step = 0
-        self.population = generate_start_population(200, 10, -50, 50) # aktualny stan algorytmu ewolucyjnego
+        self._current_step = 0
+        self._population = generate_start_population(200, 10, -50, 50) # aktualny stan algorytmu ewolucyjnego
 
         self.best_quality = 0
-        self.current_success = 0
-        self.current_distance = np.digitize(calculate_avg_distance(self.population), self.distance_bins)
-        return _to1DSpace(self.observation_dim, (self.current_success, self.current_distance)), {}
+        self._current_success = 0
+        self._current_distance = np.digitize(calculate_avg_distance(self._population), self._distance_bins)
+        return _to1DSpace(self._observation_dim, (self._current_success, self._current_distance)), {}
 
     def _calculate_reward(self, success, distance):
         reward = -1
-        if success > self.current_success:
+        if success > self._current_success:
             reward += 10
-        if distance < self.current_distance:
+        if distance < self._current_distance:
             reward += 10
         return reward
 
