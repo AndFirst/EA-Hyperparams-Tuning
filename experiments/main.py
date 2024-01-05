9651d7d32f606a5a0fce5cc213a0651af9edba43
AndFirst <irekokninski12370@gmail.com>
2024-01-05 12:54:33 +0100

    add q_data_analysis

--- experiments/main.py
+++ experiments/main.py
@@ -7,7 +7,7 @@ def calculate_base_results_statistics():
     """
     Oblicza dane statystyczne i zapisuje je do pliku csv
     """
-    with open('base_results.json', 'r') as file:
+    with open('results/base_results.json', 'r') as file:
         results = json.load(file)
     data = []
 
@@ -50,4 +50,42 @@ def calculate_q_results_statistics():
     """
     Oblicza dane statystyczne i zapisuje je do pliku csv
     """
-    pass
+    with open('results/q_results.json', 'r') as file:
+        results = json.load(file)
+    data = []
+
+    for function_name, function_values in results.items():
+        for reward_combination, values in function_values.items():
+                for repeat, value in enumerate(values, start=1):
+                    data.append((
+                        function_name,
+                        reward_combination,
+                        repeat,
+                        value
+                    ))
+    df = pd.DataFrame(
+        data, columns=['Function', 'RewardCombination', 'Repeats', 'Value'])
+    
+    sorted_df = df.sort_values(by=['Function', 'RewardCombination', 'Repeats'])
+    
+    # Grupowanie i obliczanie statystyk
+    grouped_df = sorted_df.groupby(['Function', 'RewardCombination']).agg({
+        'Value': ['mean', 'min', 'max', 'std']
+    }).reset_index()
+    
+    
+    # Zaokrąglenie wartości do 2 miejsc po przecinku
+    grouped_df.columns = [' '.join(col).strip()
+                          for col in grouped_df.columns.values]
+    grouped_df['Value mean'] = grouped_df['Value mean'].round(2)
+    grouped_df['Value min'] = grouped_df['Value min'].round(2)
+    grouped_df['Value max'] = grouped_df['Value max'].round(2)
+    grouped_df['Value std'] = grouped_df['Value std'].round(2)
+    
+    # Zapisanie do pliku csv
+    grouped_df.to_csv("results/q_data.csv", index=False)
+    
+if __name__ == "__main__":
+    calculate_base_results_statistics()
+    calculate_q_results_statistics()
+    
