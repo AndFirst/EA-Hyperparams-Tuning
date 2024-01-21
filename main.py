579c5400e49ef9ae879341c17ecdb0ba8b2ba995
iokninsk <irekokninski12370@gmail.com>
2024-01-21 09:40:45 +0100

    experiments

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from experiments.statistics import (calculate_q_results_statistics,
                                     calculate_base_results_statistics,
                                     plot_history_data_of_base_results,
                                     plot_history_data_of_q_results)
-from experiments.experiments import base_results, q_learning_results
+from experiments.experiments import base_results, q_learning_results, cross_q_table_use_results
 
 if __name__ == '__main__':
-    base_results()
-    calculate_base_results_statistics()
-    plot_history_data_of_base_results()
+    # base_results()
+    # calculate_base_results_statistics()
+    # plot_history_data_of_base_results()
 
-    q_learning_results()
-    calculate_q_results_statistics()
-    plot_history_data_of_q_results()
+    # q_learning_results()
+    # calculate_q_results_statistics()
+    # plot_history_data_of_q_results()
+
+    cross_q_table_use_results()
